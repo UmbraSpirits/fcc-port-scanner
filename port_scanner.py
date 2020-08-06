2d0d9548933a60564a6396725535a525f53003ad
UmbraSpirits <eternalmillenis@outlook.com>
2020-08-06 09:39:00 +0200

    completed project

--- port_scanner.py
+++ port_scanner.py
@@ -1,8 +1,47 @@
 import socket
-
-def get_open_ports(target, port_range):
-    open_ports = []
+from common_ports import *
 
 
+def get_open_ports(target, port_range, verbose=False):
+    url = ""
+    try:
+        url = socket.gethostbyaddr(target)
+        url = url[0]
+    except:
+        pass
+    try:
+        host = socket.gethostbyname(target)
+    except:
+        try:
+            int(target.replace(".", ""))
+            return "Error: Invalid IP address"
+        except:
+            return "Error: Invalid hostname"
+
+    port = port_range[0]
+    open_ports = ""
+    results = []
+    while port <= port_range[1]:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(0.5)
+        if s.connect_ex((host, port)):
+            pass
+        else:
+            results.append(port)
+        s.settimeout(None)
+        port += 1
+
+    if verbose == True:
+        if url == "":
+            open_ports += f"Open ports for {host}"
+        else:
+            open_ports += f"Open ports for {url} ({host})"
+        open_ports += "\nPORT     SERVICE"
+        for item in results:
+            open_ports += ("\n%-4s %-s %-s" %
+                           (str(item), "   ", ports_and_services[item]))
+
+    else:
+        open_ports = results
 
     return(open_ports)
